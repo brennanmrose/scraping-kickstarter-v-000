f24dc69b0c61dcb075977275debcb4b42ba650bf
Rose Brennan <brennan.m.rose@gmail.com>
2018-10-27 01:25:16 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,4 +1,3 @@
-require 'pry'
 require 'nokogiri'
 
 def create_project_hash
@@ -19,13 +18,3 @@ def create_project_hash
    
   projects
 end
-
-
-# create_project_hash
-
-# projects: kickstarter.css("li.project.grid_4")
-# title: project.css("h2.bbcard_name strong a").text
-# image link: project.css("div.project-thumbnail a img").attribute("src").value
-# description: project.css("p.bbcard_blurb").text
-# location: project.css("ul.project-meta span.location-name").text
-# percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
