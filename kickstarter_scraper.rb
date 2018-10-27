ebd603dcd6ec2e6bfd60746ee205759e831e090f
Rose Brennan <brennan.m.rose@gmail.com>
2018-10-27 01:04:22 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -3,7 +3,9 @@ require 'nokogiri'
 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+  binding.pry
 end
-binding.pry
+
 
 create_project_hash
